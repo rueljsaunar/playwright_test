cf80c97d66249ced78683b00880a0ace86d6d8a2
Ruel J. Saunar <rueljsaunar@yahoo.com>
2026-04-23 21:30:54 +0800

    Set to run Test only in Chromium

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,19 +35,23 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: 'chromium', // Only run tests in Chromium
+      use: { browserName: 'chromium' },
     },
+    // {
+    //   name: 'chromium',
+    //   use: { ...devices['Desktop Chrome'] },
+    // },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
